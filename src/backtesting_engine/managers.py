8e3c2d9deb51ef9841f5507fc65a80779489688f
Tom Aston <126413031+TomAston1996@users.noreply.github.com>
2025-08-11 21:46:44 +0100

    feat: add run all method in queueManager

--- src/backtesting_engine/managers.py
+++ src/backtesting_engine/managers.py
@@ -1,9 +1,10 @@
-'''
+"""
 This module manages the queue for backtesting simulations, loading configurations from a JSON file,
 and running simulations based on the specified strategies and data.
-'''
+"""
 
 import json
+import multiprocessing as mp
 
 from pathlib import Path
 
@@ -44,11 +45,51 @@ STRATEGIES = {
     "buy_and_hold": BuyAndHoldStrategy,
 }
 
+def worker(job_queue: mp.Queue[SimItem], queue_config: QueueConfig) -> None:
+    while True:
+        try:
+            sim_item = job_queue.get_nowait()
+        except Exception:
+            break  # Queue is empty
+
+        data_loader = DataLoader(cache=PersistentLRUCache())
+        data = data_loader.load(
+            ticker=sim_item.data.ticker,
+            start_date=sim_item.data.start_date,
+            end_date=sim_item.data.end_date,
+            source=sim_item.data.source,
+        )
+
+        strategy_cls = STRATEGIES[sim_item.strategy.type.lower()]
+        strategy = strategy_cls(data=data, **sim_item.strategy.fields)
+
+        engine = BTXEngine(
+            config=EngineConfig(
+                initial_cash=sim_item.sim_config.initial_cash,
+                slippage=sim_item.sim_config.slippage,
+                commission=sim_item.sim_config.commission,
+            ),
+            context=EngineContext(
+                sim_group=queue_config.sim_group,
+                sim_id=sim_item.sim_id,
+                data=data,
+                ticker=sim_item.data.ticker,
+                strategy=strategy,
+                metrics_creator=BacktestMetricCreator,
+                plot_generator=PlotGenerator,
+            ),
+        )
+
+        print(f"[{queue_config.sim_group}:{sim_item.sim_id}] Starting...")
+        engine.run_backtest()
+        print(f"[{queue_config.sim_group}:{sim_item.sim_id}] Completed.")
+
 
 class QueueManager:
     """Manages a queue loaded from a JSON file"""
 
-    def __init__(self, queue_file_path: str) -> None:
+    def __init__(self, queue_file_path: str, max_workers: int | None) -> None:
+        self.max_workers = max_workers if max_workers is not None else mp.cpu_count()
         self.queue_config = self._load_queue_config(queue_file_path=queue_file_path)
         self._create_output_directory()
 
@@ -97,7 +138,11 @@ class QueueManager:
         strategy = STRATEGIES[sim_item.strategy.type](data=data, **sim_item.strategy.fields)
 
         engine = BTXEngine(
-            config=EngineConfig(initial_cash=100_000.0, slippage=0.01, commission=0.001),
+            config=EngineConfig(
+                initial_cash=sim_item.sim_config.initial_cash,
+                slippage=sim_item.sim_config.slippage,
+                commission=sim_item.sim_config.commission,
+            ),
             context=EngineContext(
                 sim_group=self.queue_config.sim_group,
                 sim_id=sim_item.sim_id,
@@ -110,3 +155,20 @@ class QueueManager:
         )
 
         engine.run_backtest()
+
+    def run_all(self) -> None:
+        manager = mp.Manager()
+        job_queue = manager.Queue()
+
+        for sim in self.queue_config.sims:
+            job_queue.put(sim)
+
+        processes: list[mp.Process] = []
+        for _ in range(self.max_workers):
+            p = mp.Process(target=worker, args=(job_queue, self.queue_config))
+            p.start()
+            processes.append(p)
+
+        # Wait for all workers to finish
+        for p in processes:
+            p.join()
